6a9c842a46352f4beaea5847263e2efd59fd2abd
PipHarsveld <pip.harsveld@outlook.com>
2022-05-10 14:44:46 +0200

    Add images, style and scrolltrigger to 2.1

---
+++ static/javascript/script.js
@@ -0,0 +1,106 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(
+  "article:nth-of-type(4) > article:first-of-type > section:first-of-type > aside > img:first-of-type",
+  {
+    y: 800,
+    duration: 15,
+    scrollTrigger: {
+      trigger: "article:nth-of-type(4) > h2",
+      start: "top top",
+      end: "top top",
+      toggleActions: "restart none none none",
+      scrub: 2,
+    },
+  }
+);
+
+gsap.to(
+  "article:nth-of-type(4) > article:first-of-type > section:first-of-type > aside > img:nth-of-type(2)",
+  {
+    y: 835,
+    duration: 15,
+    scrollTrigger: {
+      trigger: "article:nth-of-type(4) > h2",
+      start: "top top",
+      end: "top top",
+      toggleActions: "restart none none none",
+      scrub: 2,
+    },
+  }
+);
+
+gsap.to(
+  "article:nth-of-type(4) > article:first-of-type > section:first-of-type > aside > img:nth-of-type(3)",
+  {
+    y: 948,
+    duration: 15,
+    scrollTrigger: {
+      trigger: "article:nth-of-type(4) > h2",
+      start: "top top",
+      end: "top top",
+      toggleActions: "restart none none none",
+      scrub: 2,
+    },
+  }
+);
+
+gsap.to(
+  "article:nth-of-type(4) > article:first-of-type > section:first-of-type > aside > img:nth-of-type(4)",
+  {
+    y: 907,
+    duration: 15,
+    scrollTrigger: {
+      trigger: "article:nth-of-type(4) > h2",
+      start: "top top",
+      end: "top top",
+      toggleActions: "restart none none none",
+      scrub: 2,
+    },
+  }
+);
+
+gsap.to(
+  "article:nth-of-type(4) > article:first-of-type > section:first-of-type > aside > img:nth-of-type(5)",
+  {
+    y: 955,
+    duration: 15,
+    scrollTrigger: {
+      trigger: "article:nth-of-type(4) > h2",
+      start: "top top",
+      end: "top top",
+      toggleActions: "restart none none none",
+      scrub: 2,
+    },
+  }
+);
+
+gsap.to(
+  "article:nth-of-type(4) > article:first-of-type > section:first-of-type > aside > img:nth-of-type(6)",
+  {
+    y: 954,
+    duration: 15,
+    scrollTrigger: {
+      trigger: "article:nth-of-type(4) > h2",
+      start: "top top",
+      end: "top top",
+      toggleActions: "restart none none none",
+      scrub: 2,
+    },
+  }
+);
+
+gsap.to(
+  "article:nth-of-type(4) > article:first-of-type > section:first-of-type > aside > img:last-of-type",
+  {
+    y: 1030,
+    duration: 15,
+    scrollTrigger: {
+      trigger: "article:nth-of-type(4) > h2",
+      start: "top top",
+      end: "top top",
+      toggleActions: "restart none none none",
+      scrub: 2,
+    },
+  }
+);
